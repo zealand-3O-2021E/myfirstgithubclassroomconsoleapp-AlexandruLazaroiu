02dfafac9598a35e1975eb6b619d9a31d686def2
AlexandruLazaroiu <alexandru_lazaroiu94@yahoo.com>
2021-08-31 14:38:13 +0200

    simple addition

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class Demo
+    {
+        private int x;
+        private int y;
+        private int sum;
+
+        public void Sum(int sum)
+        {
+           sum = x + y;
+           Console.WriteLine(sum);
+        }
+
+    }
+}
